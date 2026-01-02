225c1f986b5cc2d6a7f27c35bd9c1151d1ca6825
Pedro Vitor Nunes Silva <pedrovitornunes89@gmail.com>
2026-01-02 11:03:24 -0300

    Primeiro commit

---
+++ src/main/java/PedroNunesDev/Projeto_Santander/ProjetoSantanderApplication.java
@@ -0,0 +1,13 @@
+package PedroNunesDev.Projeto_Santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PedroNunesDev/Projeto_Santander/ProjetoSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package PedroNunesDev.Projeto_Santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
